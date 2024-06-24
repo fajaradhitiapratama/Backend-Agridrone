656e6052a9b832a5392bfd8133ad5d2976cac2ee
Fajar adhitia pratama <115777980+fajaradhitiapratama@users.noreply.github.com>
2024-06-24 09:54:36 +0700

    update

--- controller/basic.js
+++ controller/basic.js
@@ -27,25 +27,30 @@ exports.getHistorySoil = async function (req, res) {
     .then((snapshot) => {
       if (snapshot.exists()) {
         const data = snapshot.val();
-        const n = data.HISTORY.SOIL[id].n;
-        const p = data.HISTORY.SOIL[id].p;
-        const k = data.HISTORY.SOIL[id].k;
-        const moist = data.HISTORY.SOIL[id].moist;
-        const ph = data.HISTORY.SOIL[id].ph;
-        let status = "";
-        console.log(data.HISTORY.SOIL[id]);
-        if (ph <= 6 || moist < 0.2 || k <= 250 || p < 200 || n <= 150) {
-          status = "poor";
-        } else if (ph < 7.5 || moist < 0.6 || k <= 400 || p < 350 || n <= 200) {
-          status = "moderate";
-        } else if (ph >= 7.5 || moist <= 0.8 || k > 400 || p > 350 || n > 200) {
-          status = "good";
-        }
-
         return res.json({
           success: true,
           data: data.HISTORY.SOIL[id],
-          stat: status,
+        });
+      } else {
+        return res.json({ success: false, data: "No data available." });
+      }
+    })
+    .catch((error) => {
+      return res.json({ success: false, data: error });
+    });
+};
+
+exports.getHistoryWeather = async function (req, res) {
+  const id = req.body.id;
+  console.log(req.body);
+  firebase
+    .get(rootRef)
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        const data = snapshot.val();
+        return res.json({
+          success: true,
+          data: data.HISTORY.WEATHER[id],
         });
       } else {
         return res.json({ success: false, data: "No data available." });
--- routes/routes.js
+++ routes/routes.js
@@ -6,6 +6,7 @@ var basicControler = require('../controller/basic')
 router.get('/tampil_soil',basicControler.getSoil)
 router.post('/tampil_history_soil',basicControler.getHistorySoil)
 router.get('/fb',basicControler.masukin)
+router.post('/tampil_history_weahter',basicControler.getHistoryWeather)
 
 module.exports = router;
 
